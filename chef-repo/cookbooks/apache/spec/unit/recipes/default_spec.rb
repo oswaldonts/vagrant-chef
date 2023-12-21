b13b427d59c52ed923ccf2b9ea283e8b6f8e763a
oswaldonts <oswaldonts@proton.me>
2023-12-21 08:36:24 -0400

    update

--- chef-repo/cookbooks/apache/spec/unit/recipes/default_spec.rb
+++ chef-repo/cookbooks/apache/spec/unit/recipes/default_spec.rb
@@ -6,43 +6,75 @@
 require 'chefspec'
 
 # Cookbook:: apache
-describe 'apache::default' do
+describe 'apache2 tests' do
   let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version:'20.04').converge(described_recipe) }
 
-  #validación de paquetes necesarios para Wordpress
-  it 'installs necessary packages' do
-    expect(chef_run).to install_package('apache2')
-  end
-  
-  #validación del estado del servicio
-  it 'service status' do
-    expect(chef_run).to enable_service('apache2')
-	expect(chef_run).to start_service('apache2')
-  end
-  
-  #valida creación de archivo de configuración
-   it 'creates configuration files' do
-  expect(chef_run).to create_template('/etc/apache2/apache2.conf')
- end
-  
+  context 'with default recipe' do
+    #validación de paquetes necesarios para Wordpress
+    it 'installs necessary packages' do
+      expect(chef_run).to install_package('apache2')
+    end
 
+    #validación del estado del servicio
+    it 'is service enabled' do
+      expect(chef_run).to enable_service('apache2')
+    end
+
+#    #valida creación de archivo de configuración
+#    it 'creates configuration files' do
+#     expect(chef_run).to create_template('/etc/apache2/apache2.conf')
+#    end
+  end
 end
 
 
 # Cookbook:: php
-describe 'php::default' do
-  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version:'20.04').converge(described_recipe) } 
+describe 'php tests' do
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version:'20.04').converge(described_recipe) }
 
-#validación de paquetes necesarios para Wordpress
-  it 'installs necessary packages' do
-    expect(chef_run).to install_package('php')
+  context 'with default recipe' do
+    #validación de paquetes necesarios para Wordpress
+    it 'installs necessary packages' do
+      expect(chef_run).to install_package('php')
+    end
+
+    it 'creates configuration files' do
+      expect(chef_run).to create_template('/etc/php/php.ini')
+    end
   end
-  
- it 'creates configuration files' do
-  expect(chef_run).to create_template('/etc/php/php.ini')
- end
-  
+end
 
+# Cookbook:: mysql
+describe 'mysql tests' do
+  let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version:'20.04').converge(described_recipe) }
+
+  context 'with default recipe' do
+    it 'creates configuration files' do
+      expect(chef_run).to create_template('/opt/script.sql')
+    end
+  end
 end
 
 
+# describe 'vim_pruebas_chef::default' do
+#   platform 'ubuntu'
+#
+#   context 'with default attributes' do
+#     it "should have default install_method 'package'" do
+#       expect(chef_run.node['vim']['install_method']).to eq('package-fake')
+#     end
+#
+#     it "should include the vim_pruebas_chef::package recipe when install_method='package'" do
+#       expect(chef_run).to include_recipe('vim_pruebas_chef::package')
+#     end
+#   end
+#
+#   context "with 'source' as install_method" do
+#
+#     override_attributes['vim']['install_method'] = 'source'
+#
+#     it "should include the vim_pruebas_chef::source recipe when install_method='source'" do
+#       expect(chef_run).to include_recipe('vim_pruebas_chef::source')
+#     end
+#   end
+# end
